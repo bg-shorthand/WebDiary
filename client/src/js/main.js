1d0a00bf3602944647f98132681287fe32c979f8
GEUNA <geuna0204@gmail.com>
2021-02-02 12:21:14 +0900

    feat: add webdiary markup

--- client/src/js/main.js
+++ client/src/js/main.js
@@ -1,11 +1,11 @@
 import test1 from './test';
 import test2 from './test2';
+const axios = require('axios');
 
 test1();
 test2();
 
-fetch('http://localhost:7000/todos')
-  .then(res => res.json())
+axios.get('http://localhost:7000/todos')
   .then(res => {
-    console.log(res);
+    console.log(res.data);
   });
