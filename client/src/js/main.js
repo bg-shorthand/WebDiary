7176b73c8036d92868e0054abeb431f528ca825f
GEUNA <geuna0204@gmail.com>
2021-02-04 19:35:41 +0900

    feat: get data in read modal

--- client/src/js/main.js
+++ client/src/js/main.js
@@ -1,7 +1,10 @@
 import listRender from './main/list-render';
+import readData from './main/read-data';
+
 import modal from './modal/modal';
 
 console.log('main');
 
 listRender();
 modal();
+readData();
