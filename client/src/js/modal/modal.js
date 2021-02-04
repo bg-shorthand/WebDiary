42570b4617be0944419d9521e29bf0e740ad8b25
bg-shorthand <hanhanhan333@gmail.com>
2021-02-04 19:32:29 +0900

    feat: render cal

--- client/src/js/modal/modal.js
+++ client/src/js/modal/modal.js
@@ -1,5 +1,16 @@
 import selectModal from './select-modal';
+import calModalRender from './cal-modal-render';
+
+const $diaryCalendarPreBtn = document.querySelector('.diary-calendar__pre-btn');
+const $diaryCalendarNextBtn = document.querySelector('.diary-calendar__next-btn');
 
 export default () => {
   selectModal();
+  document.querySelector('.calendar').addEventListener('click', calModalRender(new Date()));
+
+  // $diaryCalendarPreBtn.addEventListener('click', preBtnE);
+  // $diaryCalendarNextBtn.addEventListener('click', nextBtnE);
+  // document.querySelector('.diary-calendar__pre-btn').addEventListener('click', () => {
+
+  // })
 }
