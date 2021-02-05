e7aaab165322cc67a083d86cb5aa4f72b3561fef
margu31 <judypark31@gmail.com>
2021-02-05 11:54:11 +0900

    feat: DONE! write-modal

--- client/src/js/modal/modal.js
+++ client/src/js/modal/modal.js
@@ -1,15 +1,17 @@
 import selectModal from './select-modal';
-import getDate from './write-modal';
+import { getDate, confirm } from './write-modal';
 import calModalRender from './cal-modal-render';
+import calModalPresent from './cal-modal-present';
 
 const $diaryCalendarPreBtn = document.querySelector('.diary-calendar__pre-btn');
 const $diaryCalendarNextBtn = document.querySelector('.diary-calendar__next-btn');
-import calModalPresent from './cal-modal-present';
+const $writeCompleted = document.querySelector('.write-completed');
 
 export default () => {
   console.log('modal');
   selectModal();
   getDate();
+  $writeCompleted.addEventListener('click', confirm);
   document.querySelector('.calendar').addEventListener('click', calModalRender(new Date()));
 
   // $diaryCalendarPreBtn.addEventListener('click', preBtnE);
