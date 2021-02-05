e6c005dd4fa2b68e51381d32c5d0848b86b58771
GEUNA <geuna0204@gmail.com>
2021-02-05 13:29:02 +0900

    fix: innerHtml fixed

--- client/src/js/main/read-data.js
+++ client/src/js/main/read-data.js
@@ -16,14 +16,9 @@ const diaryRender = diary => {
 
   diary.forEach(({ id, title, content }) => {
     html += `<section class="diary-content" id="${id}">
-      <h3 class="content-title" id="${id}">${title}</h3>
-      <p class="content-time" id="${id}">
-        <span class="content__year">${id}</span>
-        <span class="content__month">Feb</span>
-        <span class="content__date">1</span>
-        <span class="content__day">MON</span>
-      </p>
-      <p class="content-text" id="${id}">${content}</p>
+      <h3 class="content-title">${title}</h3>
+      <p class="content-time">${id}</p>
+      <p class="content-text">${content}</p>
       <div class="content-btn">
         <button type="button" class="content-edit far fa-edit" title="다이어리 편집하기">edit</button>
         <button type="button" class="content-close fas fa-times" title="다이어리 읽기 페이지 나가기"></button>
